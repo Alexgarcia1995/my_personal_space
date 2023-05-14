7de868ea1ea9c5c08e9ecd095910bdc77d9326da
Alejandro Garcia <alexgarciasanz1995@gmail.com>
2023-05-14 23:41:09 +0200

    Install and add test carousel

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="content">
     <Theheader/>
+    <Homecarousel/>
     <div class="container m-auto">
       <router-view></router-view>
     </div>
@@ -11,12 +12,14 @@
 <script>
 import Theheader from './components/TheHeader.vue'
 import Thefooter from './components/TheFooter.vue'
+import Homecarousel from './components/HomeCarousel.vue'
 
 export default {
   name: 'App',
   components: {
     Theheader,
-    Thefooter
+    Thefooter,
+    Homecarousel
   }
 }
 </script>
---
+++ web/src/components/HomeCarousel.vue
@@ -0,0 +1,39 @@
+<template>
+    <Carousel v-bind="settings" :autoplay="2000" :wrap-around="true">
+      <Slide v-for="(image) in images" :key="image.id">
+        <img :src="getImage(image.url)" />
+      </Slide>
+  
+      <template #addons>
+        <Pagination />
+      </template>
+    </Carousel>
+  </template>
+  
+  <script>
+  import { defineComponent } from 'vue';
+  import { Carousel, Navigation, Slide, Pagination } from 'vue3-carousel';
+  import 'vue3-carousel/dist/carousel.css';
+  export default defineComponent({
+    name: 'Homecarousel',
+    components: {
+      Pagination,
+      Carousel,
+      Slide,
+      Navigation,
+    },
+    data: () => ({
+        images: [{id: 1,url:'image1.png'}, {id: 2,url:'image1.png'}, {id: 3,url:'image1.png'}],
+        settings: {
+            itemsToShow: 1,
+            snapAlign: 'center'
+        }
+    }),
+    methods: {
+        getImage(imagePath) {
+            return new URL(`../assets/images/carousel/${imagePath}`, import.meta.url).href
+        }
+    }
+  })
+  </script>
+
